732038d392cac491bcd52eaf6a62ad4f2078d052
RS <rubenstoesser@yahoo.co.uk>
2019-04-14 21:40:52 +0200

    Updates

---
+++ precache-manifest.5c29a6e76389eb521ef8145760d863d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43aae800a8fa4cf2afeb",
+    "url": "/got-tip/static/js/runtime~main.f0bde61a.js"
+  },
+  {
+    "revision": "dfa7374f1bb569c6e4cd",
+    "url": "/got-tip/static/js/main.0b7648d2.chunk.js"
+  },
+  {
+    "revision": "384077d94407b8696b87",
+    "url": "/got-tip/static/js/2.9b0491f7.chunk.js"
+  },
+  {
+    "revision": "dfa7374f1bb569c6e4cd",
+    "url": "/got-tip/static/css/main.25640093.chunk.css"
+  },
+  {
+    "revision": "a2407742b44e77db0583b31c698dc27b",
+    "url": "/got-tip/index.html"
+  }
+];
